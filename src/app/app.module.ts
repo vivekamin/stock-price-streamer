3febb36711127bad2c5e9f963cd16e4926ca62fe
vivekamin <vivek@yodaplus.com>
2017-12-01 23:38:58 -0700

    Minor changes + Refractoring Stock Price

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { liveStockComponent } from './live-stock.component'
+import { liveStockComponent } from './live-stock/live-stock.component'
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule }   from '@angular/forms';
