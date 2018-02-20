a0c9d5d85ecebe406382f5f1803e98ab90599e1d
vivekamin <aminvivek123@gmail.com>
2018-02-20 15:05:42 -0700

    minor change

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(compression())
+app.use(compression());
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
